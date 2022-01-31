eaf33c4b17430f9adfc5c925014bfb5c46c0cfdc
husey <husey@MYC>
2022-01-31 21:10:27 +0300

    Add project files.

---
+++ Covid-19-WinForm-XML-To-DataGridView/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Covid_19_WinForm_XML_To_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.lblVaka = new System.Windows.Forms.Label();
+            this.lblOlum = new System.Windows.Forms.Label();
+            this.lblKurtarilan = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(581, 374);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(610, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 32);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Kurtarılan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(610, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 32);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ölümler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(610, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 32);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Vakalar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(610, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 32);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Test";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(610, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tatih";
+            // 
+            // lblTarih
+            // 
+            this.lblTarih.AutoSize = true;
+            this.lblTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarih.Location = new System.Drawing.Point(754, 78);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(31, 32);
+            this.lblTarih.TabIndex = 6;
+            this.lblTarih.Text = "?";
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTest.Location = new System.Drawing.Point(754, 158);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(31, 32);
+            this.lblTest.TabIndex = 7;
+            this.lblTest.Text = "?";
+            // 
+            // lblVaka
+            // 
+            this.lblVaka.AutoSize = true;
+            this.lblVaka.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblVaka.Location = new System.Drawing.Point(754, 238);
+            this.lblVaka.Name = "lblVaka";
+            this.lblVaka.Size = new System.Drawing.Size(31, 32);
+            this.lblVaka.TabIndex = 8;
+            this.lblVaka.Text = "?";
+            // 
+            // lblOlum
+            // 
+            this.lblOlum.AutoSize = true;
+            this.lblOlum.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOlum.Location = new System.Drawing.Point(754, 318);
+            this.lblOlum.Name = "lblOlum";
+            this.lblOlum.Size = new System.Drawing.Size(31, 32);
+            this.lblOlum.TabIndex = 9;
+            this.lblOlum.Text = "?";
+            // 
+            // lblKurtarilan
+            // 
+            this.lblKurtarilan.AutoSize = true;
+            this.lblKurtarilan.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKurtarilan.Location = new System.Drawing.Point(754, 398);
+            this.lblKurtarilan.Name = "lblKurtarilan";
+            this.lblKurtarilan.Size = new System.Drawing.Size(31, 32);
+            this.lblKurtarilan.TabIndex = 10;
+            this.lblKurtarilan.Text = "?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 444);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(946, 478);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblKurtarilan);
+            this.Controls.Add(this.lblOlum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblVaka);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTarih);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "COVID-19 Tablosu (XML-To-DataGridView)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTarih;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Label lblVaka;
+        private System.Windows.Forms.Label lblOlum;
+        private System.Windows.Forms.Label lblKurtarilan;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Covid-19-WinForm-XML-To-DataGridView/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace Covid_19_WinForm_XML_To_DataGridView
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             1-Dataset Veritabanınızdan bağlantısız olan çalışan bir nesnedir. Saf XML tabanlı yapısı sayesinde web formları ve DotNet componentleri ile kusursuz bir entegrasyon sağlar. 
+            Bu alandan referans alıyoruz.
+            */
+            DataSet ds = new DataSet();
+
+            /*
+             2-Asp.net ile baska bir yerden veri cekmeye çalıştığınızda hata alıyorsanız içeriği download etmeden önce aşağıdaki kodu eklemek
+             */
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            /*
+             3-XML yapısını referans aldığımız ds değişkenine tama işlemi yapıyor ve ds değişkenine xml içeriğini yüklemiş oluyoruz.
+ */
+            ds.ReadXml("https://api.memleketyazilim.com/covid-19-turkey-master/dataset/timeline.xml");
+
+            // ds değişkeni içerisindeki verileri dataGridView içerisine basıyoruz.
+            dataGridView1.DataSource = ds.Tables[0];
+
+            // O anki tarih bilgisini ve toplam data bilgisini kullanıcıya sunuyoruz.
+            label6.Text = $@"{DateTime.Now.ToShortDateString()} tarihi itibari ile toplam data sayısı {dataGridView1.RowCount.ToString()}";
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            // dataGridView her tıklandığında tıklanan alandaki değerleri ilgili labele basıyoruz.
+            lblTarih.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            lblTest.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            lblVaka.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            lblOlum.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            lblKurtarilan.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+    }
+}
